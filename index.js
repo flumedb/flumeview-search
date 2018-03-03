dfbbdc73e7c90494e192998b70ebba97fe3282b3
Dominic Tarr <dominic.tarr@gmail.com>
2018-03-03 22:19:58 +1300

    oops: fix all the copypasta errors

--- index.js
+++ index.js
@@ -9,60 +9,66 @@ function isString (s) {
 module.exports = function (version, length, map) {
   length = length || 3
   var match = /\w+/ig
-  var search = sbot._flumeUse('search', LevelView(11, function (data,seq) {
+  var create = LevelView(version, function (data, seq) {
     var words = {}
-    if(data.value.content.text) {
-      var text = data.value.content.text
+    var text = map(data)
+    if(text) {
       for(var word; word = match.exec(text); ) {
         word = word[0]
         if(word && word.length >= 3 && !words[word]) words[word] = true
       }
+      return Object.keys(words).sort().map(function (e) {
+        return [e.toLowerCase(), -seq]
+      })
     }
-    return Object.keys(words).sort().map(function (e) {
-      return [e.toLowerCase(), -data.timestamp]
-    })
-  }))
+    return []
+  })
+  return function (log, name) {
+    var index = create(log, name)
 
-  index.query = function (opts) {
-    if(isString(opts)) {
-      opts = {query: opts}
-    }
-    var pushable = Pushable(function () {
-      while(aborts.length) aborts.shift()()
-    })
-
-    var terms = opts.query.split(/\s+/)
-    var mask = 0, found = {}, ended = 0
-    var aborts = terms.filter(function (e) {
-      return e.length >= 3
-    }).map(function (term, i) {
-      mask |= 1 << i
-      var sink
-      pull(
-        search.read({
-          gt: [term, null], lte: [term, undefined],
-          keys: true, values: false
-        }),
-        sink = pull.drain(function (data) {
-          if(found[data.seq] == mask) return
-          if((found[data.seq] |= (1 << i)) == mask)
-            pushable.push(+data.seq)
-        }, function (end) {
-          if(++ ended== terms.length)
-            pushable.end()
-        })
-      )
-      return function () {
-        sink.abort(function () {})
+    index.methods = index.methods || {}
+    index.methods.query = 'source'
+    index.query = function (opts) {
+      if(isString(opts)) {
+        opts = {query: opts}
       }
-    })
-    return pull(
-      pushable,
-      opts.values !== false ? paramap(log.get): pull.through(),
-      opts.limit ? pull.take(opts.limit) : pull.through()
-    )
+      var pushable = Pushable(function () {
+        while(aborts.length) aborts.shift()()
+      })
+
+      var terms = opts.query.trim().split(/[^\w]+/)
+      var mask = 0, found = {}, ended = 0
+      var aborts = terms.filter(function (e) {
+        return e.length >= 3
+      }).map(function (term, i) {
+        mask |= 1 << i
+        var sink
+        pull(
+          index.read({
+            gt: [term, null], lte: [term+'~', undefined],
+            keys: true, values: false
+          }),
+          sink = pull.drain(function (data) {
+            if(found[data.seq] == mask) return
+            if((found[data.seq] |= (1 << i)) == mask)
+              pushable.push(+data.seq)
+          }, function (end) {
+            if(++ ended== terms.length)
+              pushable.end()
+          })
+        )
+        return function () {
+          sink.abort(function () {})
+        }
+      })
+      return pull(
+        pushable,
+        opts.values !== false ? paramap(log.get): pull.through(),
+        opts.limit ? pull.take(opts.limit) : pull.through()
+      )
+    }
+    return index
   }
 }
-
 
 
